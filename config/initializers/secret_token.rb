67848f6f4313fb7ec551ac659f6684ea6a6a5e7e
unknown <sjr@.pertronic.local>
2011-01-26 21:52:09 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7d37892478d2f3d7a48556c3766d6153c8781a1701e9bfc34469c8e596d1110a8935a6bb639d4d994c41c87607e183407073f8f47c778f098c99969469197fbf'
